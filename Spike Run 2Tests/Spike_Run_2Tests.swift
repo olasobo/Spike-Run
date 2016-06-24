b71ec1ae85e272906200dfdb6c108904f5d2e88d
Accelerator Beast <AcceleratorBeast@lives-imac-2.local>
2016-06-24 10:16:58 -0700

    Initial Commit

---
+++ Spike Run 2Tests/Spike_Run_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Spike_Run_2Tests.swift
+//  Spike Run 2Tests
+//
+//  Created by Accelerator Beast on 6/24/16.
+//  Copyright © 2016 Olatayo S. All rights reserved.
+//
+
+import XCTest
+@testable import Spike_Run_2
+
+class Spike_Run_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
